abdd32558b79ba5b627eaa8dd19a4fe0cf0c5010
dapittriandi <dpttrndi88@gmail.com>
2023-10-03 23:27:43 +0700

    add auth solve

---
+++ store/products.js
@@ -0,0 +1,177 @@
+// holds your root state
+export const state = () => ({
+  products: [
+    {
+      id: '7bada6a8-611f-49bf-83cb-e9c03441b097',
+      title: 'Warning Denimes 1.401 Pants',
+      brand: 'Warning Clothing',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/rug-1675761873564-3_grande.jpg?v=1675908315',
+      harga: 439,
+      rating: null,
+      created_at: '2023-09-26T16:16:31.596804+00:00',
+      detailProducts: null
+    },
+    {
+      id: '47f596a0-3ffe-4d86-a61d-eec3ba237abc',
+      title: 'Warning Denimes 1.397 Pants',
+      brand: 'Warning Clothing',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/rug-1675761491400-0_grande.jpg?v=1675827476',
+      harga: 439,
+      rating: null,
+      created_at: '2023-09-26T16:21:22.106466+00:00',
+      detailProducts: null
+    },
+    {
+      id: '675b73c6-65a5-4648-afcc-4c30d807a0a4',
+      title: 'Warning Denimes 1.388 Pants\n\n',
+      brand: 'warning clothing',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/DENIMES1.3881_1_grande.jpg?v=1672911970',
+      harga: 439,
+      rating: null,
+      created_at: '2023-09-26T14:49:35.549062+00:00',
+      detailProducts: null
+    },
+    {
+      id: '9151a785-6d90-4124-aa50-7bc539bc4c94',
+      title: 'Warning Denimes 1.396 Pants',
+      brand: 'Warning Clothing',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/rug-1675762049839-0_1048x.jpg?v=1675908378',
+      harga: 439,
+      rating: null,
+      created_at: '2023-09-26T16:17:50.806006+00:00',
+      detailProducts: null
+    },
+    {
+      id: 'e6ab99c0-ead2-42ae-af6b-f24f925bd6d2',
+      title: 'Warning Denimes 1.390 Pants',
+      brand: 'Warning Clothing',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/DENIMES1.3901_1_grande.jpg?v=1672905489',
+      harga: 439,
+      rating: 'NULL',
+      created_at: '2023-09-26T18:14:07.406863+00:00',
+      detailProducts: null
+    },
+    {
+      id: 'c5a7957c-b906-4af9-898d-fb59527cb187',
+      title: 'Warning Denimes 1.390 Pants jeans',
+      brand: 'Warning Clothing jambi',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/DENIMES1.3901_1_grande.jpg?v=1672905489',
+      harga: 439,
+      rating: '',
+      created_at: '2023-09-28T17:46:26.751083+00:00',
+      detailProducts: null
+    },
+    {
+      id: '417aae08-1768-4012-a058-3c8a6c171542',
+      title: 'Warning Denimes 1.390 Pants jeans',
+      brand: 'Warning Clothing jambi',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/DENIMES1.3901_1_grande.jpg?v=1672905489',
+      harga: 439,
+      rating: '',
+      created_at: '2023-09-28T17:46:26.751083+00:00',
+      detailProducts: null
+    },
+    {
+      id: 'f494e3de-ffad-49e3-ba21-09bbc2aa4970',
+      title: 'Warning Denimes 1.390 Pants jeans',
+      brand: 'Warning Clothing jambi',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/DENIMES1.3901_1_grande.jpg?v=1672905489',
+      harga: 439,
+      rating: '',
+      created_at: '2023-09-28T17:46:26.751083+00:00',
+      detailProducts: null
+    },
+    {
+      id: 'db9f195f-cefb-42ca-9f04-dd5a038a856e',
+      title: 'Warning Denimes 1.390 Pants jeans',
+      brand: 'Warning Clothing jambi',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/DENIMES1.3901_1_grande.jpg?v=1672905489',
+      harga: 439,
+      rating: '',
+      created_at: '2023-09-28T17:46:26.751083+00:00',
+      detailProducts: null
+    },
+    {
+      id: '60ab3356-4372-4a31-bf67-1bc53e3bb86a',
+      title: 'Warning Denimes 1.390 Pants jeans',
+      brand: 'Warning Clothing jambi',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/DENIMES1.3901_1_grande.jpg?v=1672905489',
+      harga: 439,
+      rating: '',
+      created_at: '2023-09-28T17:46:26.751083+00:00',
+      detailProducts: null
+    },
+    {
+      id: '54c9b08d-27ac-41bc-a9ef-6c037523cc8a',
+      title: 'Warning Denimes 1.390 Pants jeans',
+      brand: 'Warning Clothing jambi',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/DENIMES1.3901_1_grande.jpg?v=1672905489',
+      harga: 439,
+      rating: '',
+      created_at: '2023-09-28T17:46:26.751083+00:00',
+      detailProducts: null
+    },
+    {
+      id: '8d255005-0918-46ac-a73d-6c5e4c672e3f',
+      title: 'Warning Denimes 1.390 Pants jeans',
+      brand: 'Warning Clothing jambi',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/DENIMES1.3901_1_grande.jpg?v=1672905489',
+      harga: 439,
+      rating: '',
+      created_at: '2023-09-28T17:46:26.751083+00:00',
+      detailProducts: null
+    },
+    {
+      id: '1f180d0c-faf9-444e-8663-b1528fdf65d5',
+      title: 'Warning Denimes 1.390 Pants jeans',
+      brand: 'Warning Clothing jambi',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/DENIMES1.3901_1_grande.jpg?v=1672905489',
+      harga: 439,
+      rating: '',
+      created_at: '2023-09-28T17:46:26.751083+00:00',
+      detailProducts: null
+    },
+    {
+      id: '1779050c-0d81-42f9-ad1f-d538ecf5d587',
+      title: 'Warning Denimes 1.390 Pants jeans',
+      brand: 'Warning Clothing jambi',
+      gambar:
+        'https://www.warning-clothing.com/cdn/shop/products/DENIMES1.3901_1_grande.jpg?v=1672905489',
+      harga: 439,
+      rating: '',
+      created_at: '2023-09-28T17:46:26.751083+00:00',
+      detailProducts: null
+    }
+  ]
+})
+// contains your actions
+export const actions = {
+  counterUp ({ state, commit }) {
+    commit('setCounter', state.counter + 1)
+  }
+}
+// contains your mutations
+export const mutations = {
+  setCounter (state, value) {
+    state.counter = value
+  }
+}
+// your root getters
+export const getters = {
+  myGetter (state) {
+    return state.counter + 1000
+  }
+}
